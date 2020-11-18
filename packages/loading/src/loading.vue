2958f23c65dd7ad7738cf4278112530b5bbc6e72
pengxuesong <pxs_623@163.com>
2020-11-18 09:31:02 +0800

    修改loading

--- packages/loading/src/loading.vue
+++ packages/loading/src/loading.vue
@@ -10,7 +10,7 @@
           <circle class="path" cx="50" cy="50" r="20" fill="none"/>
         </svg>
         <i v-else :class="spinner"></i>
-        <p v-if="text" class="el-loading-text">{{ text }}</p>
+        <p v-if="text" class="el-loading-text" v-html="text"></p>
       </div>
     </div>
   </transition>
